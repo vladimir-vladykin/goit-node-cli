2a718f845f13e4199215f384b25f51ec70024862
Volodymyr Vladykin <vova@pocketguard.com>
2025-11-06 22:33:35 +0200

    Setup basic code for CLI application

---
+++ contacts.js
@@ -0,0 +1,25 @@
+// contacts.js
+
+import { promises as fs } from "fs";
+const contactsPath = "./db/contacts.json";
+
+async function listContacts() {
+  // ...твій код. Повертає масив контактів.
+  return [];
+}
+
+async function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+async function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+export {
+  listContacts
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+import { program } from "commander";
+import { listContacts } from "./contacts.js";
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+// TODO: рефакторити
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      let contacts = await listContacts();
+      console.log("Contacts list:");
+      console.log(contacts);
+      break;
+
+    case "get":
+      console.log("This is get");
+      break;
+
+    case "add":
+      console.log("This is add");
+      break;
+
+    case "remove":
+      console.log("This is remove by id");
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
